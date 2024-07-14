7d227ffbb0f11ab49365b08bfe9d018b66b8ce70
UADokKi <nazar8rusuch@gmail.com>
2024-07-14 09:24:55 +0100

    Start

--- app/src/main/java/com/example/readingdiaryapp/MainActivity.kt
+++ app/src/main/java/com/example/readingdiaryapp/MainActivity.kt
@@ -3,8 +3,11 @@ package com.example.readingdiaryapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.ui.Modifier
 import com.example.readingdiaryapp.ui.theme.ReadingDiaryAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -12,8 +15,8 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             ReadingDiaryAppTheme {
-                Surface(color = MaterialTheme.colors.background) {
-                    // Main content goes here
+                Surface(color = MaterialTheme.colors.background, modifier = Modifier.fillMaxSize()) {
+                    Text("Hello, Reading Diary App!")
                 }
             }
         }
